dfbb5ebb7a2e3caae9f32ede01b425304f60d0dd
Jakub Czyz <me@jakubc.pl>
2025-08-19 22:14:57 +0200

    add oh my posh config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 rm -rf ~/.config/hypr/custom.d && ln -s $(pwd)/hypr/custom.d ~/.config/hypr/custom.d
 rm -rf ~/.config/kitty && ln -s  $(pwd)/kitty ~/.config/kitty
+rm -rf ~/.config/ohmyposh && ln -s  $(pwd)/ohmyposh ~/.config/ohmyposh
 rm -rf ~/.config/paru && ln -s  $(pwd)/paru ~/.config/paru
 rm -rf ~/.config/wallpapers && ln -s $(pwd)/wallpapers ~/.config/wallpapers
 rm -rf ~/.config/zshrc && ln -s $(pwd)/zshrc ~/.config/zshrc
