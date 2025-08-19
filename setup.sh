f124644a9a31d711bbcbc64676c10d9ba4204072
Jakub Czyz <me@jakubc.pl>
2025-08-19 21:08:52 +0200

    add paru and update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm -rf ~/.config/hypr/custom.d && ln -s ./hypr/custom.d ~/.config/hypr/custom.d
-rm -rf ~/.config/kitty && ln -s  ./kitty ~/.config/kitty
-rm -rf ~/.config/wallpapers && ln -s ./wallpapers ~/.config/wallpapers
-rm -rf ~/.config/zshrc && ln -s ~/zshrc ~/.config/zshrc
-rm -rf ~/.zshrc && ln -s .zshrc ~/.zshrc
+rm -rf ~/.config/hypr/custom.d && ln -s $(pwd)/hypr/custom.d ~/.config/hypr/custom.d
+rm -rf ~/.config/kitty && ln -s  $(pwd)/kitty ~/.config/kitty
+rm -rf ~/.config/paru && ln -s  $(pwd)/paru ~/.config/paru
+rm -rf ~/.config/wallpapers && ln -s $(pwd)/wallpapers ~/.config/wallpapers
+rm -rf ~/.config/zshrc && ln -s $(pwd)/zshrc ~/.config/zshrc
+rm -rf ~/.zshrc && ln -s $(pwd)/.zshrc ~/.zshrc
