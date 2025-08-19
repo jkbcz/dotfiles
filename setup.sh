503d1db332d453948aab275dc4165ff14921f664
Jakub Czyz <me@jakubc.pl>
2025-08-19 20:57:35 +0200

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm ~/.config/hypr/config.d && ln -s ./hypr/config.d ~/.config/hypr/config.d
-rm ~/.config/kitty && ln -s  ./kitty ~/.config/kitty
-rm ~/.config/wallpapers && ln -s ./wallpapers ~/.config/wallpapers
-rm ~/.config/zshrc && ln -s ~/zshrc ~/.config/zshrc
-rm ~/.zshrc && ln -s .zshrc ~/.zshrc
+rm -f ~/.config/hypr/custom.d && ln -s ./hypr/custom.d ~/.config/hypr/custom.d
+rm -f ~/.config/kitty && ln -s  ./kitty ~/.config/kitty
+rm -f ~/.config/wallpapers && ln -s ./wallpapers ~/.config/wallpapers
+rm -f ~/.config/zshrc && ln -s ~/zshrc ~/.config/zshrc
+rm -f ~/.zshrc && ln -s .zshrc ~/.zshrc
